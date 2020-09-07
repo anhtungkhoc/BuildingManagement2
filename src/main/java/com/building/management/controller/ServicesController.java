4a0a33b54886c3a188d8867ddaf87b673c5719b8
anhtungkhoc <trinhdai15@gmail.com>
2020-09-07 16:21:02 +0700

    version 2.0 : full services part 2

--- src/main/java/com/building/management/controller/ServicesController.java
+++ src/main/java/com/building/management/controller/ServicesController.java
@@ -22,37 +22,38 @@ public class ServicesController {
     public ServicesController(BMService<Services> serService) {this.serService = serService; }
 
     @GetMapping("/all") //Method tra ve list model
-    public Iterable<Services> getListCompany(){
+    public Iterable<Services> getListServices(){
         return serService.findAll();
     }
 
     @GetMapping("/{MA_DV}") //Method tra ve 1 model theo id
-    public Services companyById(@PathVariable("MA_DV") String MA_DV) {
-        Optional<Services> opCompany = serService.findById(MA_DV);
-        if (opCompany.isPresent()) {
-            return opCompany.get();
+    public Services servicesById(@PathVariable("MA_DV") String MA_DV) {
+        Optional<Services> opServices = serService.findById(MA_DV);
+        if (opServices.isPresent()) {
+            return opServices.get();
         }
         return null;
     }
     @GetMapping("/search") //url "/search?keyword={name}: name phai viet dung ky tu hoa thuong theo ten cua model de lay ra thong tin cua model chua keyword nay
-    public List<Services> companyByKeyword(@RequestParam(name ="keyword", required = false, defaultValue = "") String name) {
+    public List<Services> servicesByKeyword(@RequestParam(name ="keyword", required = false, defaultValue = "") String name) {
         return serService.searchByKeyWord(name);
     }
 
     @PostMapping(consumes = "application/json") //Create mot model moi vao database
     @ResponseStatus(HttpStatus.CREATED)
-    public Services postCompany(@RequestBody Services company) {
-        return serService.save(company);
+    public Services postServices(@RequestBody Services services) {
+        return serService.save(services);
     }
 
     @PutMapping("/{MA_DV}")
     @ResponseStatus(HttpStatus.CREATED) //Update mot model vao database
-    public Services putCompany(@RequestBody Services company) {
-        return serService.save(company);
+    public Services putServices(@RequestBody Services services) {
+
+        return serService.save(services);
     }
 
     @DeleteMapping("/{MA_DV}") //Xoa mot model theo ID
-    public void deleteCompany(@PathVariable("MA_DV") String MA_DV) {
+    public void deleteServices(@PathVariable("MA_DV") String MA_DV) {
         try {
             serService.deleteById(MA_DV);
         } catch (EmptyResultDataAccessException e) {
